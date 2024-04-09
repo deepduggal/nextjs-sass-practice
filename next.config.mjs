a008fc51d2a85fa330205738b6a9aa069438bb38
Deep Duggal <7544238+deepduggal@users.noreply.github.com>
2024-04-08 20:51:47 -0400

    Added 7-in-1 architecture folders. Sass setup

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+import { fileURLToPath } from 'url';
+import { join, dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const nextConfig = {
+  sassOptions: {
+    includePaths: [join(__dirname, 'styles')],
+  }
+};
 
 export default nextConfig;
